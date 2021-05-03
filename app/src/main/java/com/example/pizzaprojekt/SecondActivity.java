f14f2d28e7daa6f4586dbb973863e02abb353ea9
vagnerdavid <vagnerdavid00@gmail.com>
2021-05-03 19:26:39 +0200

    Spinner, pizza méret választó

--- app/src/main/java/com/example/pizzaprojekt/SecondActivity.java
+++ app/src/main/java/com/example/pizzaprojekt/SecondActivity.java
@@ -2,14 +2,40 @@ package com.example.pizzaprojekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    int currentItem = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        TextView valasszon = findViewById(R.id.valasszon);
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+
+
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(currentItem==position){
+                    return;
+                }else{
+                    Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
